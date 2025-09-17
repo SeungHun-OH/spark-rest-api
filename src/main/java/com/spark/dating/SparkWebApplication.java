e60ac96f1ebd1356c8d2f18c2f1da8e689a73726
SeungHun-OH <oh_0209@naver.com>
2025-09-17 19:58:21 +0900

    프로젝트를 위한 첫 번째 커밋

---
+++ src/main/java/com/spark/dating/SparkWebApplication.java
@@ -0,0 +1,13 @@
+package com.spark.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spark/dating/SparkWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
