bada82b18c87c230256ddfbf2ab8b1903cfcefd5
Smk <thsaudrhks1@gmail.com>
2025-09-20 22:33:42 +0900

    1

--- src/main/java/com/spark/dating/member/MemberDao.java
+++ src/main/java/com/spark/dating/member/MemberDao.java
@@ -2,7 +2,11 @@ package com.spark.dating.member;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.spark.dating.dto.member.Member;
+
 @Mapper
 public interface MemberDao {
-    
+  int insertMember(Member member);
+
+  Member SelectMemberByM_id(String m_id);  
 }
