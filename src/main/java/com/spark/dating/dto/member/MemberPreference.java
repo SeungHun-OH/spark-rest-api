116768a78e469d26edd27408fbb16a6f5ba6f958
Smk <thsaudrhks1@gmail.com>
2025-09-24 15:35:12 +0900

    수정

---
+++ src/main/java/com/spark/dating/dto/member/MemberPreference.java
@@ -0,0 +1,19 @@
+package com.spark.dating.dto.member;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MemberPreference {
+  //MemberPreference 테이블명 참조 부모키만 가짐
+  private int mp_no;
+  private int mp_memberno;
+  private int mp_preferno;
+  private Date created_at; 
+
+  List<PreferenceCategory>SelfPrefers;
+  List<PreferenceCategory>PartnerPrefers;
+
+}
---
+++ src/main/java/com/spark/dating/dto/member/PreferenceCategory.java
@@ -0,0 +1,14 @@
+package com.spark.dating.dto.member;
+
+import lombok.Data;
+
+@Data
+public class PreferenceCategory {
+
+  private int    pc_no;
+  private String pc_type;
+  private int    pc_typenum;
+  private String pc_group;
+  private String pc_name;
+
+}
