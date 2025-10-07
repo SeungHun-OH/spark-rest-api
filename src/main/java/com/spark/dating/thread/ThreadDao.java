0bd0f14a68565d1886b9c56292af69599064a61b
Smk <thsaudrhks1@gmail.com>
2025-10-07 18:35:19 +0900

    댓글도 불러오기는 했으나... picture 칼럼 일리야스 조인문에서

--- src/main/java/com/spark/dating/thread/ThreadDao.java
+++ src/main/java/com/spark/dating/thread/ThreadDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.spark.dating.dto.thread.BoardReply;
 import com.spark.dating.dto.thread.ThreadBoard;
 import com.spark.dating.dto.thread.response.ThreadBoardResponse;
 
